6d3ba603c0b106ea10c67af83734573cc662e5c3
iamakam <akamhiwa90@gmail.com>
2024-02-24 13:37:26 +0300

    navbar2

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -34,14 +34,14 @@ function Header({fetchUrl,children}) {
   
   return (
     <>
-     <div className=' w-full h-[550px] md:h-[600px] text-white '>
+     <div className=' w-full h-[700px] md:h-[772px] pt-[5rem] text-white relative'>
       <div className='w-full h-full'>
-        <div className='absolute w-full h-[550px] md:h-[600px] bg-gradient-to-tr from-black'></div>
+        <div className='absolute w-full top-[5rem] bottom-0 bg-gradient-to-tr from-black'></div>
           <img src={`${ImageBaseUrl}${RandomMovie?.backdrop_path}`} alt={RandomMovie?.name} className='h-full w-full object-cover ' />
-          <div className='absolute top-[20%] md:top-[25%] py-4 px-7 md:py-8 '>
+          <div className='absolute top-[35%] md:top-[30%] py-4 px-7 md:py-8 '>
                 <h1 className='text-3xl md:text-5xl font-bold py-2'>{RandomMovie?.title || RandomMovie?.name}</h1>
                 <p className='text-gray-100 xl:max-w-[40%] lg:max-w-[50%] md:max-w-[70%]'>{ truncate(RandomMovie?.overview ,200)}</p>
-                <p className='text-gray-400  text-small pt-0.5'>Released {RandomMovie?.release_date || RandomMovie?.first_air_date}</p>
+                <p className='text-gray-400  text-small pt-0.5'>Released {RandomMovie?.release_date || RandomMovie?.first_air_date}</p >
                 <div className=' my-4'>
                   <button className='text-black py-2 px-6 border-gray-300 bg-gray-300 border'>play</button>
                   <button className='text-white border border-gray-300 py-2 px-6 ml-4'>Watch trailer</button>
--- src/assets/Nav.jsx
+++ src/assets/Nav.jsx
@@ -1,18 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
 function Nav({onOpen}) {
-  const [nav,setNav]=useState(false)
-      useEffect(()=>{
-        const addScrool=()=>{
-          window.scrollY>300 ?setNav(true):setNav(false)
-        }
-        document.addEventListener("scroll",addScrool)
-
-        return ()=>document.removeEventListener("scroll",addScrool)
-      },[window.scrollY])
   return (
-    <div className={
-      `flex h-20 fixed top-0 right-0 left-0 items-center px-7 pr-20 z-20 ${nav ? `bg-black`:`bg-black/40`}  py-2`}>
+    <div className={`flex h-20 fixed top-0 right-0 left-0 items-center px-7 md:pr-20 z-20 bg-black  py-2`}>
       <div className='flex justify-between w-full items-center' >
       <h1 className=' text-red-600 text-4xl font-bold italic cursor-pointer'>NETFLIX</h1>
       <ul>
